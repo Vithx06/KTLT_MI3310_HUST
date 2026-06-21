62afad48fc9c69951e173e8b831ff2b6753d3a5d
Thế Vinh <91892754+Vithx06@users.noreply.github.com>
2026-06-21 10:16:57 +0700

    Add files via upload

---
+++ PJ_DiemDanh/include/diemDanh.h
@@ -0,0 +1,14 @@
+#ifndef DIEMDANH_H
+#define DIEMDANH_H
+
+#include "structs.h"
+
+void ghiNhanDiemDanh(LopHoc dsLop[], int soLuongLop);
+
+void xemDiemDanhLop();
+
+void capNhatDiemDanh();
+
+void moduleDiemDanh(LopHoc dsLop[], int soLuongLop);
+
+#endif
